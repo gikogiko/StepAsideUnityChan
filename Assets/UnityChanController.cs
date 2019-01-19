0e736110251fe1b860fff61eedb93d65cedabbd6
gikogiko <blend8e@gmail.com>
2019-01-19 22:26:25 +0900

    Lesson6 対応内容 ・Init comit

---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour
+{
+    //アニメーションするためのコンポーネントを入れる
+    private Animator myAnimator;
+    //Unityちゃんを移動させるコンポーネントを入れる（追加）
+    private Rigidbody myRigidbody;
+    //前進するための力
+    private float forwardForce = 800.0f;
+    // 左右に移動するための力
+    private float turnForce = 500.0f;
+    //ジャンプするための力
+    private float upForce = 500.0f;
+    // 左右に移動するための力
+    private float movableRange = 3.4f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //アニメータコンポーネントを取得
+        this.myAnimator = GetComponent<Animator>();
+
+        //走るアニメーションを開始
+        this.myAnimator.SetFloat("Speed", 1);
+
+        //Rigidbodyコンポーネントを取得
+        this.myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Unityちゃんに前方向の力を加える
+        this.myRigidbody.AddForce(this.transform.forward * this.forwardForce);
+
+        Debug.Log("移動" + this.transform.position.x);
+        //Unityちゃんを矢印キーまたはボタンに応じて左右に移動させる（追加）
+        if (Input.GetKey(KeyCode.LeftArrow) && -this.movableRange < this.transform.position.x)
+        {
+            //左に移動（追加）
+            this.myRigidbody.AddForce(-this.turnForce, 0, 0);
+            Debug.Log("左に移動" + this.transform.position.x);
+            Debug.Log("turnForce：左" + this.turnForce);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) && this.transform.position.x < this.movableRange)
+        {
+            //右に移動（追加）
+            this.myRigidbody.AddForce(this.turnForce, 0, 0);
+            Debug.Log("右に移動" + this.transform.position.x );
+            Debug.Log("turnForce：右" + this.turnForce);
+        }
+
+        //Jumpステートの場合はJumpにfalseをセットする
+        if (this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
+        {
+            this.myAnimator.SetBool("Jump", false);
+        }
+
+        //ジャンプしていない時にスペースが押されたらジャンプする
+        if (Input.GetKeyDown(KeyCode.Space) && this.transform.position.y < 0.5f)
+        {
+            //ジャンプアニメを再生
+            this.myAnimator.SetBool("Jump", true);
+            //Unityちゃんに上方向の力を加える
+            this.myRigidbody.AddForce(this.transform.up * this.upForce);
+        }
+
+    }
+}
