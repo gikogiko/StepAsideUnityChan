4b4494aa3b57a8c930a590ffc1d854a7340a4b59
gikogiko <blend8e@gmail.com>
2019-01-27 17:39:19 +0900

    Lesson6 課題 対応内容 ・DeletePassedObjectスクリプトを障害物オブジェクトにアタッチ。

---
+++ Assets/DeletePassedObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeletePassedObject : MonoBehaviour {
+
+	private GameObject characterObj;
+	//
+
+	// Use this for initialization
+	void Start () {
+		characterObj = GameObject.Find("unitychan");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.gameObject.transform.position.z < characterObj.transform.position.z){
+            Destroy(this.gameObject);
+        }
+	}
+}
